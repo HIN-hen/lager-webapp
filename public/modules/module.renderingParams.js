53a52349aab8b8715614ef0a591d9f9b7e386d9d
HENNLICH\HIN <harald.innetzberger@hennlich.at>
2024-11-19 10:07:18 +0100

    feat: adapt canvas to changed videoContainer dimensions automatically

--- public/modules/module.renderingParams.js
+++ public/modules/module.renderingParams.js
@@ -8,6 +8,19 @@ const noActiveStream = document.querySelector('.no-active-stream');
 // Adapted width, height of video, canvas
 const videoContainer = document.querySelector('#video-container');
 let { width, height } = videoContainer.getBoundingClientRect();
+console.log("orWidth: " + width, ", orHeight: " + height);
+
+const resizeObserver = new ResizeObserver((entries) => {
+    const elementSize = entries[0];
+    const { height, width } = elementSize.contentRect;
+    console.log(width, height);
+    canvas.height = height;
+    canvas.width = width;
+});
+resizeObserver.observe(videoContainer);
+
+console.log(canvas);
+console.log("cWidth: " + canvas.width, "cHeight: " + canvas.height);
 
 export { 
     width, 
