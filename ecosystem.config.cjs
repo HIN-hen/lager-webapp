7b1aad7a5e5488a4d331bc14a494b34a1225e581
HENNLICH\HIN <harald.innetzberger@hennlich.at>
2024-11-28 14:48:09 +0100

    no message

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -15,16 +15,30 @@ module.exports = {
       },
     ],
     deploy: {
-      production: {
+      development: {
         user: 'lager',
-        //host: 'lpa.hennlich.at',
-        host: '10.100.120.56',
+        host: '127.0.0.1',
+        //host: '10.100.120.56',
         key: 'deploy.key',
         ref: 'origin/main',
         repo: 'https://github.com/HIN-hen/lager-webapp.git',
         path: '/home/lager',
         'post-deploy':
-          'npm install && pm2 reload ecosystem.config.cjs && pm2 save && git checkout package.lock',
+          'npm install && pm2 reload && pm2 save && git checkout package.lock',
+        env: {
+          NODE_ENV: 'development',
+        },
+      },
+      production: {
+        user: 'lager',
+        host: 'lpa.hennlich.at',
+        //host: '10.100.120.56',
+        key: 'deploy.key',
+        ref: 'origin/main',
+        repo: 'https://github.com/HIN-hen/lager-webapp.git',
+        path: '/home/lager',
+        'post-deploy':
+          'npm install && pm2 reload && pm2 save && git checkout package.lock',
         env: {
           NODE_ENV: 'production',
         },
